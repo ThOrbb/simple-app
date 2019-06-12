04c5cc0df2c4251a03755848fea75b410cb5787f
Alex <adkosmachev@edu.hse.ru>
2019-06-12 19:30:24 +0300

    akjkad

---
+++ app.py
@@ -0,0 +1,17 @@
+import json
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "Hello"
+
+
+@app.route("/alive")
+def alive():
+    return json.dumps({'alive': True})
+
+
+app.run('0.0.0.0', 8000)
