ff6b10b4e3c42f28943f84aa908aa7daf23ccbab
Alexey Kosmachev <aleksey.kosmachev@bostongene.com>
2019-06-16 18:44:41 +0300

    add deploy script

--- app.py
+++ app.py
@@ -4,9 +4,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
-    return "Hello1"
+    return "Hello12"
 
 
 @app.route("/alive")
--- fabfile.py
+++ fabfile.py
@@ -1,5 +1,6 @@
 import os
 
+from patchwork import files
 from fabric import Connection, Config, task
 
 
@@ -8,20 +9,23 @@ def deploy(c):
     remote_user = os.environ['REMOTEUSER']
     remote_pass = os.environ['USERPASS']
     host = os.environ['REMOTEHOST']
-    port = os.environ['PORT']
 
     config = Config(overrides={'sudo': {'password': remote_pass}})
     connect_kwargs = {"password": remote_pass, 'allow_agent': False}
     with Connection(host=host, user=remote_user, config=config, connect_kwargs=connect_kwargs) as conn:
         print("Connected with remote server")
+
         print("Copy sources")
         conn.put("app.py")
         conn.put("requirements.txt")
+
         print("Install requirements")
         conn.sudo("pip3 install -r requirements.txt")
-        print("Run server")
-        if fabric.contrib.files.exists("/root/server.pid"):
-            print("ASDHJKFDSHG")
 
-        conn.sudo("nohup python3 app.py & echo $! > pid")
+        if files.exists(conn, "/root/server.pid"):
+            print("Shutdown previous server")
+            conn.sudo("pkill -F server.pid")
+
+        print("Run server")
+        conn.sudo("nohup python3 app.py &> /dev/null & echo $! > server.pid")
         print("Success!")
